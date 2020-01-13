1f06c837bcacf354fec351a92f74fc403c9d9303
Jack Valley <Juuso.laakso@turku.fi>
2020-01-13 14:59:16 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap/";
+import { Router } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <header className="App-header"></header>
-        <h1>Turku Asukasportaali</h1>
-        <Row>
-          <Col class="col1">
-            <h3>1 of 2</h3>
-          </Col>
-          <Col>
-            <h3>2 of 2</h3>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h3>1 of 3</h3>
-          </Col>
-          <Col>
-            <h3>2 of 3</h3>
-          </Col>
-          <Col>
-            <h3>3 of 3</h3>
-          </Col>
-        </Row>
-      </Container>
+      <Router>
+        <Container>
+          <Navbar />
+          <Row>
+            <Col class="col1">
+              <h3>1 of 2</h3>
+            </Col>
+            <Col>
+              <h3>2 of 2</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h3>1 of 3</h3>
+            </Col>
+            <Col>
+              <h3>2 of 3</h3>
+            </Col>
+            <Col>
+              <h3>3 of 3</h3>
+            </Col>
+          </Row>
+        </Container>
+      </Router>
     </div>
   );
 }
