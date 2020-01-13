e13388dc703df2737bd8b8e8c0fcac099b42ee0f
Jere Kyyrö <jere.kyyro@turku.fi>
2020-01-13 14:32:35 +0200

    Update App.js
    added columns and rows etc.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
 import "./App.css";
+import { Container, Row, Col } from "react-bootstrap/";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Container>
+        <header className="App-header"></header>
         <h1>Turku Asukasportaali</h1>
-      </header>
+        <Row>
+          <Col class="col1">
+            <h3>1 of 2</h3>
+          </Col>
+          <Col>
+            <h3>2 of 2</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>1 of 3</h3>
+          </Col>
+          <Col>
+            <h3>2 of 3</h3>
+          </Col>
+          <Col>
+            <h3>3 of 3</h3>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
