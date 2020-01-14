b7210c0e2a1b80de234cc2f2b9529c92b8606f74
tmokko <58773830+tmokko@users.noreply.github.com>
2020-01-14 10:44:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,79 +1,14 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap/";
 import Navbar from "./components/layout/Navbar";
+import Boxes from "./Boxes";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <div>
-        <Container>
-          <Row className="row">
-            <Col className="col1" xs={5}>
-              <h3>1 of 1</h3>
-              <p>
-                nd typesetting industry. Lorem Ipsum has been the industry's
-                standard dummy text ever since the 1500s, when an unknown
-                printer took a galley of type and scrambled it to make a type
-                specimen book. It has survived not only five centuries, but also
-                the leap into electronic typesetting, remaining essentially
-                unchanged. It was popularised in the 1960s with the release of
-                Letraset sheets containing Lorem Ipsum passages, and more
-                recently with desktop publishing software like Aldus PageMaker
-                including versions of Lorem Ipsum.
-              </p>
-            </Col>
-            <Col className="col1" xs={5}>
-              <h3>2 of 1</h3>
-              <p>
-                nd typesetting industry. Lorem Ipsum has been the industry's
-                standard dummy text ever since the 1500s, when an unknown
-                printer took a galley of type and scrambled it to make a type
-                specimen book. It has survived not only five centuries, but also
-                the leap into electro
-              </p>
-            </Col>
-          </Row>
-          <Row className="row">
-            <Col className="col1" xs={5}>
-              <h3>1 of 2</h3>
-              <p>
-                nd typesetting industry. Lorem Ipsum has been the industry's
-                standard dummy text ever since the 1500s, when an unknown
-                printer took a galley of type and scrambled it to make a type
-                specimen book.
-              </p>
-            </Col>
-            <Col className="col1" xs={5}>
-              <h3>2 of 2</h3>
-              <p>
-                nd typesetting industry. Lorem Ipsum has been the industry's sts
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
-            </Col>
-          </Row>
-          <Row className="row">
-            <Col className="col1" xs={5}>
-              <h3>1 of 3</h3>
-              <p>
-                nd typesetting industry. Lorem Ipsum has been the industry's
-                standard dummy text ever since the 1500s, when an unknown
-                printer took a galley of type and scrambled it to make a type
-                specimen book. It has survived not only five centuries, but also
-                the leap into electronic typesetting, remaining essentially
-                unchanged. It was popularised in the 1960s with the release of
-                Letraset sheets containing Lorem Ipsum passages, and more
-                recently with desktop publishing software like Aldus PageMaker
-                including versions of Lorem Ipsum.
-              </p>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Boxes></Boxes>
+      <div></div>
     </div>
   );
 }
