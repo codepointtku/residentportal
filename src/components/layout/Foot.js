5faba80c431695289bb287bf0d887c8975ed0f0e
DelusionCB <threepiiem@gmail.com>
2020-01-14 12:21:41 +0200

    Footer update

--- src/components/layout/Foot.js
+++ src/components/layout/Foot.js
@@ -13,6 +13,22 @@ const Foot = () => {
             <img alt="Turun kaupunki" src={turkulogo} title="Turun kaupunki" />
           </div>
         </div>
+        <div class="col-lg-6 col-md-6" id="footID">
+    <h2 id="footh2">Varaamo</h2>
+    <p><span id="footspan">© Turun Kaupunki 
+        <br />PL 355 20101 Turku 
+        <br />vaihde (02) 330 000 
+        <br />
+        <a href="http://www.turku.fi" rel="noopener noreferrer" target="_blank">www.turku.fi</a>
+    </span>
+    <br />
+    <br />
+    <a class="accessibility-info-link" href="/accessibility-info">Saavutettavuusseloste</a>
+    <br />
+    <a class="feedback-link" href="https://opaskartta.turku.fi/eFeedback/fi/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut" rel="noopener noreferrer" target="_blank">Palautteesi voit lähettää täältä.
+                </a>
+              </p>
+            </div>
       </div>
     </div>
    
