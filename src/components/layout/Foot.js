ee7cfc059ae7c4cdba4ca21bb8c83cf905293035
Jack Valley <Juusto.laakso@gmail.com>
2020-01-14 16:01:30 +0200

    Update to Footer

--- src/components/layout/Foot.js
+++ src/components/layout/Foot.js
@@ -5,45 +5,39 @@ import turkulogo from "./turkufooterlogo2.png";
 const Foot = () => {
   return (
     <div className="footer">
-      <div className="row">
-        <div className="container col-lg-3 col-md-3">
-          <div className="container col-lg-6 col-md-6">
-            <h2 id="footh2">Turku Asukasportaali</h2>
-            <p>
-              <span id="footspan">
-                © Turun Kaupunki
-                <br />
-                PL 355 20101 Turku
-                <br />
-                vaihde (02) 330 000
-                <br />
-                <a
-                  href="http://www.turku.fi"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  www.turku.fi
-                </a>
-              </span>
+      <div className="container">
+        <div className="container">
+          <h2 id="footh2">Turku Asukasportaali</h2>
+          <p>
+            <span id="footspan">
+              © Turun Kaupunki
               <br />
+              PL 355 20101 Turku
               <br />
-              <a href="/">
-                Saavutettavuusseloste
-              </a>
+              vaihde (02) 330 000
               <br />
               <a
-                href="https://opaskartta.turku.fi/eFeedback/fi/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut"
+                href="http://www.turku.fi"
+                rel="noopener noreferrer"
+                target="_blank"
               >
-                Palautteesi voit lähettää täältä.
+                www.turku.fi
               </a>
-            </p>
-            <img
-              alt="Turun kaupunki"
-              id="footerlogo"
-              src={turkulogo}
-              title="Turun kaupunki"
-            />
-          </div>
+            </span>
+            <br />
+            <br />
+            <a href="/">Saavutettavuusseloste</a>
+            <br />
+            <a href="https://opaskartta.turku.fi/eFeedback/fi/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut">
+              Palautteesi voit lähettää täältä.
+            </a>
+          </p>
+          <img
+            alt="Turun kaupunki"
+            id="footerlogo"
+            src={turkulogo}
+            title="Turun kaupunki"
+          />
         </div>
       </div>
     </div>
