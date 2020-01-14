2f2b25f2950d9206271de9c22e79af17d7123f1e
Jack Valley <Juusto.laakso@gmail.com>
2020-01-14 14:46:14 +0200

    Update to Footer

--- src/components/layout/Foot.js
+++ src/components/layout/Foot.js
@@ -1,37 +1,55 @@
 import React from "react";
 import {} from "react-bootstrap";
-import turkulogo from './turkufooterlogo2.png';
-
+import turkulogo from "./turkufooterlogo2.png";
 
 const Foot = () => {
   return (
-    
     <div className="container footer">
-      <div className="row" id="footrow">
+      <div className="row footrow">
         <div className="col-lg-3 col-md-3">
-          <div>
-            <img alt="Turun kaupunki" id="footerlogo" src={turkulogo} title="Turun kaupunki" />
+          <div className="col-lg-6 col-md-6 footID">
+            <h2 id="footh2">Turku Asukasportaali</h2>
+            <p>
+              <span id="footspan">
+                © Turun Kaupunki
+                <br />
+                PL 355 20101 Turku
+                <br />
+                vaihde (02) 330 000
+                <br />
+                <a
+                  href="http://www.turku.fi"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  www.turku.fi
+                </a>
+              </span>
+              <br />
+              <br />
+              <a className="accessibility-info-link" href="/accessibility-info">
+                Saavutettavuusseloste
+              </a>
+              <br />
+              <a
+                className="feedback-link"
+                href="https://opaskartta.turku.fi/eFeedback/fi/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Palautteesi voit lähettää täältä.
+              </a>
+            </p>
+            <img
+              alt="Turun kaupunki"
+              id="footerlogo"
+              src={turkulogo}
+              title="Turun kaupunki"
+            />
           </div>
         </div>
-        <div className="col-lg-6 col-md-6" id="footID">
-    <h2 id="footh2">Turku Asukasportaali</h2>
-    <p><span id="footspan">© Turun Kaupunki 
-        <br />PL 355 20101 Turku 
-        <br />vaihde (02) 330 000 
-        <br />
-        <a href="http://www.turku.fi" rel="noopener noreferrer" target="_blank">www.turku.fi</a>
-    </span>
-    <br />
-    <br />
-    <a className="accessibility-info-link" href="/accessibility-info">Saavutettavuusseloste</a>
-    <br />
-    <a className="feedback-link" href="https://opaskartta.turku.fi/eFeedback/fi/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut" rel="noopener noreferrer" target="_blank">Palautteesi voit lähettää täältä.
-                </a>
-              </p>
-            </div>
       </div>
     </div>
-   
   );
 };
 export default Foot;
