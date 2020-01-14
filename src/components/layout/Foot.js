c18c195d0bfd34b0bc7cd4210b86d0c481f05398
Jack Valley <Juusto.laakso@gmail.com>
2020-01-14 12:32:54 +0200

    Update to Footer

--- src/components/layout/Foot.js
+++ src/components/layout/Foot.js
@@ -9,11 +9,11 @@ const Foot = () => {
     <div className="container footer">
       <div className="row">
         <div className="col-lg-3 col-md-3">
-          <div className="navbar-brand">
+          <div>
             <img alt="Turun kaupunki" id="footerlogo" src={turkulogo} title="Turun kaupunki" />
           </div>
         </div>
-        <div class="col-lg-6 col-md-6" id="footID">
+        <div className="col-lg-6 col-md-6" id="footID">
     <h2 id="footh2">Turku Asukasportaali</h2>
     <p><span id="footspan">© Turun Kaupunki 
         <br />PL 355 20101 Turku 
@@ -23,9 +23,9 @@ const Foot = () => {
     </span>
     <br />
     <br />
-    <a class="accessibility-info-link" href="/accessibility-info">Saavutettavuusseloste</a>
+    <a className="accessibility-info-link" href="/accessibility-info">Saavutettavuusseloste</a>
     <br />
-    <a class="feedback-link" href="https://opaskartta.turku.fi/eFeedback/fi/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut" rel="noopener noreferrer" target="_blank">Palautteesi voit lähettää täältä.
+    <a className="feedback-link" href="https://opaskartta.turku.fi/eFeedback/fi/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut" rel="noopener noreferrer" target="_blank">Palautteesi voit lähettää täältä.
                 </a>
               </p>
             </div>
