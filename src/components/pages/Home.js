1a7a4686949be3ca3c0a86e500a4b681372e270a
Jack Valley <Juuso.laakso@turku.fi>
2020-01-15 10:06:09 +0200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,16 +3,18 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container">
-      <h1 className="text-light">
-        Asukasportaali - <br></br>Kaikki tietosi samassa paikassa
-      </h1>
-      <h5 className="text-light  ">
-        Asukasportaalista voit tarkistaa esimerkiksi kirjastolainasi,<br></br>
-        lääkäriasiointisi ja bussikorttisi voimassaoloajat.
-      </h5>
-      <button type="button" className="btn btn-dark text-uppercase">
-        Kirjaudu asukasportaaliin
-      </button>
+      <div className="text-center">
+        <h1 className="text-light">
+          Asukasportaali - <br></br>Kaikki tietosi samassa paikassa
+        </h1>
+        <h5 className="text-light  ">
+          Asukasportaalista voit tarkistaa esimerkiksi kirjastolainasi,<br></br>
+          lääkäriasiointisi ja bussikorttisi voimassaoloajat.
+        </h5>
+        <button type="button" className="btn btn-dark text-uppercase">
+          Kirjaudu asukasportaaliin
+        </button>
+      </div>
     </div>
   );
 };
