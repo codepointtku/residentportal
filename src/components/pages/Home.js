f125f815ad536370fe92a9d58ca12c49520e327a
Jack Valley <Juuso.laakso@turku.fi>
2020-01-15 11:53:34 +0200

    Update to Home styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,19 +2,26 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="container ">
-      <div className="text-left ml-1 mt-5">
-        <h1 className="text-light ml-1">
-          Asukasportaali - <br></br>Kaikki tietosi samassa paikassa
-        </h1>
+    <div className="p-5 mt-5">
+      <div className="container">
+        <div className="text-left ml-1 mt-5">
+          <h1 className="text-light ml-1">
+            Asukasportaali - <br></br>Kaikki tietosi samassa paikassa
+          </h1>
 
-        <h5 className="text-light ml-1 mt-4">
-          Asukasportaalista voit tarkistaa esimerkiksi kirjastolainasi,<br></br>
-          lääkäriasiointisi ja bussikorttisi voimassaoloajat.
-        </h5>
-        <button type="button" className="ml-1 mt-5 btn btn-dark text-uppercase">
-          Kirjaudu asukasportaaliin
-        </button>
+          <h5 className="text-light ml-1 mt-4">
+            Asukasportaalista voit tarkistaa esimerkiksi kirjastolainasi,
+            <br></br>
+            lääkäriasiointisi ja bussikorttisi voimassaoloajat.
+          </h5>
+          <button
+            type="button"
+            className="ml-1 mt-5 btn btn-dark text-uppercase"
+          >
+            Kirjaudu asukasportaaliin
+          </button>
+          <div className="p-5 mt-5"></div>
+        </div>
       </div>
     </div>
   );
