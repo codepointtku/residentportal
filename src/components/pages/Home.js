f33ddfb872f353818d88f5618fdc84f6df30dd5f
Jack Valley <Juuso.laakso@turku.fi>
2020-01-15 10:20:34 +0200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container ">
-      <div classname="text-center">
+      <div className="text-left">
         <h1 className="text-light">
           Asukasportaali - <br></br>Kaikki tietosi samassa paikassa
         </h1>
