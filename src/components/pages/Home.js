7f753c46b205d90659a37ae093aee6a96faa89d0
tmokko <58773830+tmokko@users.noreply.github.com>
2020-01-15 10:15:41 +0200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,16 +2,20 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="container">
-      <h1 className="text-light">Asukasportaali - <br></br>Kaikki tietosi samassa paikassa</h1>
-      <h2 className="text-light "></h2>
-      <h5 className="text-light  ">
-        Asukasportaalista voit tarkistaa esimerkiksi kirjastolainasi,<br></br>
-        lääkäriasiointisi ja bussikorttisi voimassaoloajat.
-      </h5>
-      <button type="button" class="btn btn-dark text-uppercase">
-        Kirjaudu asukasportaaliin
-      </button>
+    <div className="container ">
+      <div classname="text-center">
+        <h1 className="text-light">
+          Asukasportaali - <br></br>Kaikki tietosi samassa paikassa
+        </h1>
+
+        <h5 className="text-light  ">
+          Asukasportaalista voit tarkistaa esimerkiksi kirjastolainasi,<br></br>
+          lääkäriasiointisi ja bussikorttisi voimassaoloajat.
+        </h5>
+        <button type="button" class="btn btn-dark text-uppercase">
+          Kirjaudu asukasportaaliin
+        </button>
+      </div>
     </div>
   );
 };
