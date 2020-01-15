257e822c1455be3ba656a50a259015e0bb47c272
Jack Valley <Juuso.laakso@turku.fi>
2020-01-15 09:41:29 +0200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,13 +3,14 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container">
-      <h1 className="text-light">Asukasportaali - <br></br>Kaikki tietosi samassa paikassa</h1>
-      <h2 className="text-light "></h2>
+      <h1 className="text-light">
+        Asukasportaali - <br></br>Kaikki tietosi samassa paikassa
+      </h1>
       <h5 className="text-light  ">
         Asukasportaalista voit tarkistaa esimerkiksi kirjastolainasi,<br></br>
         lääkäriasiointisi ja bussikorttisi voimassaoloajat.
       </h5>
-      <button type="button" class="btn btn-dark text-uppercase">
+      <button type="button" className="btn btn-dark text-uppercase">
         Kirjaudu asukasportaaliin
       </button>
     </div>
