71847e3e8ef42812661abb3852a90a8718ff65c0
Jack Valley <Juusto.laakso@gmail.com>
2020-01-20 10:34:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./App.scss";
 import Home from "./components/pages/Home";
 import Residentalportal from "./components/pages/Residentportal";
 import About from "./components/pages/About";
-import Aspa from "./components/pages/Aspa"
+import Aspa from "./components/pages/Aspa";
 
 function App() {
   return (
@@ -20,6 +20,9 @@ function App() {
         <Route exact path="/" component={Home} />
         <Switch>
           <Route path="/residentportal" component={NavbarDark} />
+        </Switch>
+        <Switch>
+          <Route path="/aspa" component={NavbarDark} />
         </Switch>
         <Switch>
           <Route exact path="/aspa" component={Aspa} />
