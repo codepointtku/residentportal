5791836bc4cb9905ef1deb486083adef14623913
jerezzz <jere.kyyro@gmail.com>
2020-01-22 10:09:26 +0200

    Put components into separate files

---
+++ src/components/layout/ContrastButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import contrastButtonBlack from "./images/contrast_button.png";
+import contrastButtonWhite from "./images/contrast_button_white.png";
+
+function ContrastButton(props) {
+  const bgColor = props.bgcolor;
+  if (bgColor === "dark") {
+    return <img src={contrastButtonWhite} alt="contrast button" />;
+  }
+  return <img src={contrastButtonBlack} alt="contrast button" />;
+}
+
+export default ContrastButton;
