813141a9d69853049bab28f07dcb59581b7fda6f
Ducky07 <Juuso.laakso@turku.fi>
2020-02-07 15:23:42 +0200

    Update Header.js

--- src/components/chatwindow/Header.js
+++ src/components/chatwindow/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
       <div className="sc-header">
         <img className="sc-header--img" src={this.props.imageUrl} alt="" />
         <div className="sc-header--team-name"> {this.props.teamName} </div>
-        <div className="barcontainer">
+        <div className="barcontainer" onClick={this.props.onClose}>
           <div className="bar1" onClick={this.props.onClose}></div>
           <div className="bar2" onClick={this.props.onClose}></div>
         </div>
